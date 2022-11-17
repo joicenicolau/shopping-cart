9cce62226eedfac8cf7d57a5994e5e90db5c33cb
Joice Nicolau <joice_nicolau@hotmail.com>
2022-11-16 21:49:11 -0300

    Resolvendo requisito 4

--- src/main.js
+++ src/main.js
@@ -6,11 +6,24 @@ import { createProductElement } from './helpers/shopFunctions';
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const getClassProduct = document.querySelector('.products');
-const amazenaFetchProductsList = await fetchProductsList('computador');
+const criaTextoCarregando = () => {
+  const criaParagrafo = document.createElement('p');
+  criaParagrafo.innerText = 'carregando...';
+  criaParagrafo.className = 'loading';
+  getClassProduct.appendChild(criaParagrafo);
+};
+criaTextoCarregando();
 
+const amazenaFetchProductsList = await fetchProductsList('computador');
 const functionCreateList = async () => {
   amazenaFetchProductsList.forEach((el) => {
     getClassProduct.appendChild(createProductElement(el));
   });
 };
 functionCreateList();
+
+const removeLoadingClass = () => {
+  const apagaClassLoading = document.querySelector('.loading');
+  apagaClassLoading.remove();
+};
+removeLoadingClass();
